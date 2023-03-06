f229feffae49176472095c99e3560ea8ae702c7a
Suyash K <suyu162001@gmail.com>
2023-03-06 14:38:46 +0530

    feat: Add Custom Command and Test
    - [x] create custom command to wait for db
    - [x] create test-cases to test the command
    - [x] implement wait_for_db command
    - [x] update docker-compose.yml and .github/workflows/checks.yml

---
+++ src/core/management/commands/wait_for_db.py
@@ -0,0 +1,29 @@
+"""
+WaitForDb: Django Command
+- waits for db to be available
+"""
+
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+  """
+  Django Command to wait for db
+  """
+  def handle(self, *args, **options):
+    """
+    Entrypoint for custom command
+    """
+    self.stdout.write("[LOG] Waiting for database...")
+    db_up = False
+    while db_up is False:
+      try:
+        self.check(databases=['default'])
+        db_up = True
+      except (Psycopg2OpError, OperationalError):
+        self.stdout.write("[LOG] Database Unavailable. Retry in 1 second...")
+        time.sleep(1)
+    self.stdout.write(self.style.SUCCESS("[LOG] Database Available!"))
